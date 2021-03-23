0d19878c14271b59dc0bb9a7f42572510720fa4e
zhangrunyu <runyu12345@163.com>
2021-03-24 06:27:29 +0800

    依赖来源.

--- ioc-container-overview/src/main/java/com/runyu/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
+++ ioc-container-overview/src/main/java/com/runyu/thinking/in/spring/ioc/overview/dependency/injection/DependencyInjectionDemo.java
@@ -4,6 +4,7 @@ import com.runyu.thinking.in.spring.ioc.overview.repository.UserRepository;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.env.Environment;
 
 /**
  * 依赖查找示例
@@ -16,6 +17,7 @@ public class DependencyInjectionDemo {
         // 配置 XML 配置文件  objectFactory
         // 启动应用上下文
         BeanFactory beanFactory = new ClassPathXmlApplicationContext("classpath:/META-INF/dependency-injection-context.xml");
+        // 自定义Bean
         UserRepository userRepository =  beanFactory.getBean(UserRepository.class);
         //System.out.println(userRepository.getUsers());
         // 注入的 beanFactory 和 实际的主方法 beanFactory 不是一个对象
@@ -26,6 +28,16 @@ public class DependencyInjectionDemo {
         // System.out.println(userRepository.getBeanFactory() == beanFactory);
         //System.out.println(beanFactory.getBean(BeanFactory.class)); // 查找(错误代码)
 
+        // 容器自建Bean
+        Environment environment = beanFactory.getBean(Environment.class);
+        System.out.println(environment);
+
+        /**
+         * 依赖来源
+         * 1. 自定义Bean
+         * 2. 容器内建的 Bean 对象
+         * 3. 容器内建依赖
+         */
     }
 
 }
